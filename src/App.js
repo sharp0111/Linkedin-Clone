4a01445082c7d8331d6a97993043c459174f22f9
Res Yeg <shaqk702@gmail.com>
2018-02-07 20:36:24 -0800

    navbar is done

--- src/App.js
+++ src/App.js
@@ -9,17 +9,14 @@ import postData from './data.js';
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-      <div>Linkedin MyNetwork Page Clone is under construction.</div>
-      <div><NavBar/></div>
+      <div className='app__container'>
+        <NavBar />
+        <div className='body__container'>
+          {/* <LeftBar />
+          <MainContent />
+          <RightBar /> */}
+        </div>
+      </div>
     );
   }
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import styles from './NavBar.css';
+import linkedinLogo from './if_Popular_Social_Media-22_2329259.png';
+import homeIcon from './home-icon.png';
 
 class NavBar extends Component {
 
 	render() {
 		return (
-			<div className="NavBar">
-                <label>Who are you looking for?</label>
-				<input className="NavBarSearch-field" type="text" onChange={this.submitFilter} ref={input => this.input = input} placeholder="Search"></input>
-			</div>
+			<nav className="NavBar">
+                <img src={linkedinLogo} alt="LinkedIn logo" height="46" width="46"></img>
+				<input className="NavBarSearch-field" type="text" onChange={this.submitFilter} placeholder="&#x1F50D; Search"></input>
+                
+                <div className="menu__buttons">
+                    <a href='#'><img src={homeIcon} height="46" width="46" alt='button1'/></a>
+                    <a href='#'><img src={homeIcon} height="46" width="46" alt='button2'/></a>
+                    <a href='#'><img src={homeIcon} height="46" width="46" alt='button3'/></a>
+                    <a href='#'><img src={homeIcon} height="46" width="46" alt='button4'/></a>
+                </div>
+			</nav>
 		);
 	}
 };
