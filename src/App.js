e100d0984a72a572299774ae1371fb38881417f2
Res Yeg <shaqk702@gmail.com>
2018-02-08 17:31:22 -0800

    rearranged folders

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import NavBar from './components/NavBar/NavBar.js';
-import Card from './components/Card/Card.js';
+import NavBar from './NavBar/NavBar';
+import Card from './Card/Card';
 
 import postData from './data.js';
 
 class App extends Component {
+
   render() {
     return (
       <div className='app__container'>
         <NavBar />
         <div className='body__container'>
+          {/* <LeftRail />
           <Card />
+          <Banner /> */}
         </div>
       </div>
     );
